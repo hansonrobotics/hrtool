27ecf320112fb97b617abd731489a2c9cbb57b2b
Wenwei Huang <dut.hww@gmail.com>
2017-05-24 17:53:30 +0800

    Install newer hrtool

--- get_hr.bash
+++ get_hr.bash
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-version=0.2.6
-curl -sLo /tmp/head-hr_${version}_amd64.deb https://github.com/hansonrobotics/hrtool/releases/download/v${version}/head-hr_${version}_amd64.deb
+version=v0.3.12
+curl -sLo /tmp/head-hr_${version}_amd64.deb https://github.com/hansonrobotics/hrtool/releases/download/${version}/head-hr_${version#v}_amd64.deb
 if [[ -f /usr/local/bin/hr-base ]]; then
     sudo rm /usr/local/bin/hr
     sudo rm /usr/local/bin/hr-base
     sudo rm /usr/local/bin/hr-ext
 fi
-sudo dpkg -i /tmp/head-hr_${version}_amd64.deb
+sudo dpkg -i /tmp/head-hr_${version}_amd64.deb || sudo apt-get install -f
 rm /tmp/head-hr_${version}_amd64.deb
