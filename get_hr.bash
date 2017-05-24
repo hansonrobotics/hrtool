436391dec53933ae5352b0159e7e5bfffcf27272
Wenwei Huang <dut.hww@gmail.com>
2017-05-24 17:57:17 +0800

    No prompt

--- get_hr.bash
+++ get_hr.bash
@@ -7,5 +7,5 @@ if [[ -f /usr/local/bin/hr-base ]]; then
     sudo rm /usr/local/bin/hr-base
     sudo rm /usr/local/bin/hr-ext
 fi
-sudo dpkg -i /tmp/head-hr_${version}_amd64.deb || sudo apt-get install -f
+sudo dpkg -i /tmp/head-hr_${version}_amd64.deb || sudo apt-get install -yf
 rm /tmp/head-hr_${version}_amd64.deb
