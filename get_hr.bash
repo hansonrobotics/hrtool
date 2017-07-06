e0230dafecf7b4c10f848e3c85885aeec8adc2a1
Wenwei <wenwei-dev@users.noreply.github.com>
2017-07-06 15:32:40 +0800

    Update get_hr.bash

--- get_hr.bash
+++ get_hr.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-version=v0.4.1
+version=v0.6.1
 curl -sLo /tmp/head-hr_${version}_amd64.deb https://github.com/hansonrobotics/hrtool/releases/download/${version}/head-hr_${version#v}_amd64.deb
 if [[ -f /usr/local/bin/hr-base ]]; then
     sudo rm /usr/local/bin/hr
