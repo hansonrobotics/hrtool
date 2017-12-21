f06ea23c409674218a8ce06870b5603a3f6a3823
Wenwei Huang <dut.hww@gmail.com>
2017-12-21 17:47:35 +0800

    Install after confirm

--- checkupdate.sh
+++ checkupdate.sh
@@ -1,15 +1,22 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 newest_ver=$(hr cmd get_latest_version hansonrobotics/hrtool)
 curr_ver=$(dpkg-query -W -f='${Version}\n' head-hr)
 if dpkg --compare-versions $curr_ver lt ${newest_ver#v}; then
-    zenity --title "New version" --info \
-        --text="There is head-hr available. Run \"hr install head-hr\" to upgrade it"
+    if zenity --question --title "New Version Available" --text="Newer version of head-hr ($newest_ver) is available. Do you want to update?"; then
+        echo "Update head-hr from \"${curr_ver}\" to \"${newest_ver#v}\""
+        hr install head-hr
+    fi
 else
+    echo "No update for head-hr available"
     newest_ext_ver=$(hr cmd get_latest_version hansonrobotics/hrtool-ext)
     curr_ext_ver=$(dpkg-query -W -f='${Version}\n' head-hr-ext)
     if dpkg --compare-versions $curr_ext_ver lt ${newest_ext_ver#v}; then
-        zenity --title "New version" --info \
-            --text="There is new head-hr-ext available. Run \"hr install head-hr-ext\" to upgrade it"
+        if zenity --question --title "New Version Available" --text="Newer version of head-hr-ext ($newest_ext_ver) is available. Do you want to update?"; then
+            echo "Update head-hr-ext from \"${curr_ext_ver}\" to \"${newest_ext_ver#v}\""
+            hr install head-hr-ext
+        fi
+    else
+        echo "No update for head-hr-ext available"
     fi
 fi
