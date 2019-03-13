c2ea5c416b3b0f5d77868597c7aa54ab7cfb6369
Wenwei Huang <dut.hww@gmail.com>
2019-03-13 13:08:35 +0800

    sudo command doesn't exist on docker

--- get_hr.bash
+++ get_hr.bash
@@ -13,11 +13,15 @@ elif [[ $OS_VERSION == 'xenial' ]]; then
     fi
 fi
 
+SUDO=""
+if [[ $(id -u) != 0 ]]; then
+    SUDO="sudo"
+fi
 curl -sLo /tmp/head-hr_${version}_amd64.deb https://github.com/hansonrobotics/hrtool/releases/download/${version}/head-hr_${version#v}_amd64.deb
 if [[ -f /usr/local/bin/hr-base ]]; then
-    sudo rm /usr/local/bin/hr
-    sudo rm /usr/local/bin/hr-base
-    sudo rm /usr/local/bin/hr-ext
+    $SUDO rm /usr/local/bin/hr
+    $SUDO rm /usr/local/bin/hr-base
+    $SUDO rm /usr/local/bin/hr-ext
 fi
-sudo dpkg -i /tmp/head-hr_${version}_amd64.deb || sudo apt-get install -yf
+$SUDO dpkg -i /tmp/head-hr_${version}_amd64.deb || $SUDO apt-get install -yf
 rm /tmp/head-hr_${version}_amd64.deb
